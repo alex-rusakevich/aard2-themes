c7db6a713b003dfe25e8870f4cf237fce89cd9b6
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2024-05-08 19:23:39 +0800

    #patch added some comments

--- a2tg/utils.py
+++ a2tg/utils.py
@@ -1,7 +1,16 @@
 import os
 
 
-def fallback_get(dic, *args):
+def fallback_get(dic: dict, *args: str) -> any:
+    """Try to get element from dict using keys. If key 1 failed, try get by key 2 etc.
+
+    :param dic: python dictionary structure
+    :type dic: dict
+    :raises Exception: None of the keys was found in dictionary
+    :return: value in dict found
+    :rtype: any
+    """
+
     for arg in args:
         if arg in dic:
             return dic[arg]
@@ -9,7 +18,15 @@ def fallback_get(dic, *args):
     raise Exception(f"None of keys {args} was found in dictionary")
 
 
-def gen_injection(inj_name) -> str:
+def gen_injection(inj_name: str) -> str:
+    """Get injection (.css data) by injection name. Looks for .css files in config folder
+
+    :param inj_name: css injection name
+    :type inj_name: str
+    :return: css data
+    :rtype: str
+    """
+
     if not inj_name:
         return ""
 
